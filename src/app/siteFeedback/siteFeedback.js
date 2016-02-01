ef4d3210db4af16ed9286126dc00f76cf959f84f
bvkrylov <bvkrylov@ua.edu>
2016-02-01 12:16:28 -0600

    Testing WP-API on dev2

--- src/app/siteFeedback/siteFeedback.js
+++ src/app/siteFeedback/siteFeedback.js
@@ -4,10 +4,19 @@ angular.module('manage.siteFeedback', [])
             $scope.responses = [];
             $scope.userInfo = {};
 
+            console.log("checking current user...");
             wpTestFactory.getCurrentUser()
                 .success(function(data) {
                     console.dir(data);
                     $scope.userInfo = data;
+                    console.log("retrieving current user details...");
+                    wpTestFactory.getUserDetails($scope.userInfo.id)
+                        .success(function(data) {
+                            console.dir(data);
+                        })
+                        .error(function(data, status, headers, config) {
+                            console.log(data);
+                        });
                 })
                 .error(function(data, status, headers, config) {
                     console.log(data);
