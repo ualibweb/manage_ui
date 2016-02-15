55f74ca659b86fb1c16b9745090bdd41b1a4f95b
bvkrylov <bvkrylov@ua.edu>
2016-02-15 17:26:48 -0600

    fix 35

--- dist/manage.js
+++ dist/manage.js
@@ -3199,10 +3199,11 @@ angular.module('common.manage', [])
                     url: API + 'users',
                     method: 'GET',
                     transformResponse: appendTransform($http.defaults.transformResponse, function (data) {
-                        console.dir(data);
-                        var stripHTML = data.replace(/<\/?[^>]+(>|$)/g, "");
-                        console.log(stripHTML);
-                        return stripHTML;
+                        if (typeof data === 'string') {
+                            data = angular.fromJson(data.replace(/<\/?[^>]+(>|$)/g, ""));
+                            console.dir(data);
+                        }
+                        return data;
                     })
                 })
             }
