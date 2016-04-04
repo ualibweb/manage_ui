fa5a53bd653ca2b920617cc3a17c7d6c1c52d942
bvkrylov <bvkrylov@ua.edu>
2016-04-04 12:35:54 -0500

    OneSearch errors graph test 2

--- dist/manage.js
+++ dist/manage.js
@@ -2447,7 +2447,16 @@ angular.module("oneSearchErrors/oneSearchErrors.tpl.html", []).run(["$templateCa
     "        <label class=\"btn btn-primary\" ng-model=\"engine\" uib-btn-radio=\"2\">eJournals</label>\n" +
     "    </div>\n" +
     "    <div class=\"row\" ng-repeat=\"year in errors.tree[engine]\">\n" +
-    "        <h4>{{$index}}</h4>\n" +
+    "        <h4><a ng-click=\"year.open = !year.open\">{year.name}}</a><small>{{year.counter}}</small></h4>\n" +
+    "        <div class=\"col-xs-12\" ng-repeat=\"month in year.months\" ng-if=\"year.open\">\n" +
+    "            <h5><a ng-click=\"month.open = !month.open\">{month.name}}</a><small>{{month.counter}}</small></h5>\n" +
+    "            <div class=\"col-xs-12\" ng-repeat=\"day in month.days\" ng-if=\"month.open\">\n" +
+    "                <h6><a ng-click=\"day.open = !day.open\">{day.day}}</a><small>{{day.counter}}</small></h6>\n" +
+    "                <span class=\"label label-info\" ng-repeat=\"error in day.errors\" ng-if=\"day.open\">\n" +
+    "                    {{error}}\n" +
+    "                </span>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
     "    </div>\n" +
     "</div>");
 }]);
@@ -6645,6 +6654,7 @@ angular.module('manage.oneSearchErrors', ['oc.lazyLoad'])
 
         errorsFactory.getData()
             .success(function(data) {
+                var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
                 var today = new Date();
                 var tree = [];
                 for (var j = 0; j < 3; j++) {
@@ -6676,19 +6686,60 @@ angular.module('manage.oneSearchErrors', ['oc.lazyLoad'])
                     for (i = 0; i < curData.length; i++) {
                         curData[i] = curData[i].replace(/-/g,'/');
                         var dt = new Date(curData[i]);
-                        if (!angular.isDefined(tree[j][dt.getFullYear()])) {
-                            tree[j][dt.getFullYear()] = [];
-                            tree[j][dt.getFullYear()].open = false;
+                        var isPresent = false;
+                        var y = 0, m = 0, d = 0;
+                        for (y = 0; y < tree[j].length; y++) {
+                            if (tree[j][y].year === dt.getFullYear()) {
+                                isPresent = true;
+                                break;
+                            }
                         }
-                        if (!angular.isDefined(tree[j][dt.getFullYear()][dt.getMonth()])) {
-                            tree[j][dt.getFullYear()][dt.getMonth()] = [];
-                            tree[j][dt.getFullYear()][dt.getMonth()].open = false;
+                        if (!isPresent) {
+                            var year = {};
+                            year.open = false;
+                            year.counter = 0;
+                            year.name = dt.getFullYear();
+                            year.months = [];
+                            tree[j].push(year);
+                            y = tree[j].length - 1;
                         }
-                        if (!angular.isDefined(tree[j][dt.getFullYear()][dt.getMonth()][dt.getDate()])) {
-                            tree[j][dt.getFullYear()][dt.getMonth()][dt.getDate()] = {open: false, counter: 0, errors: []};
+                        isPresent = false;
+                        for (m = 0; m < tree[j][y].months.length; m++) {
+                            if (tree[j][y].months[m].mm === dt.getMonth()) {
+                                isPresent = true;
+                                break;
+                            }
                         }
-                        tree[j][dt.getFullYear()][dt.getMonth()][dt.getDate()]['counter']++;
-                        tree[j][dt.getFullYear()][dt.getMonth()][dt.getDate()]['errors'].push(dt);
+                        if (!isPresent) {
+                            var month = {};
+                            month.open = false;
+                            month.counter = 0;
+                            month.mm = dt.getMonth();
+                            month.name = months[month.mm];
+                            month.days = [];
+                            tree[j][y].months.push(month);
+                            m = tree[j][y].months.length - 1;
+                        }
+                        isPresent = false;
+                        for (d = 0; d < tree[j][y].months[m].days.length; d++) {
+                            if (tree[j][y].months[m].days[d].day === dt.getDate()) {
+                                isPresent = true;
+                                break;
+                            }
+                        }
+                        if (!isPresent) {
+                            var day = {};
+                            day.open = false;
+                            day.day = dt.getDate();
+                            day.counter = 0;
+                            day.errors = [];
+                            tree[j][y].months[m].days.push(day);
+                            d = tree[j][y].months[m].days.length - 1;
+                        }
+                        tree[j][y].counter++;
+                        tree[j][y].months[m].counter++;
+                        tree[j][y].months[m].days[d].counter++;
+                        tree[j][y].months[m].days[d].errors.push(dt);
 
                         if (dt.getFullYear() === today.getFullYear()) {
                             $scope.errors.mapped.year[j][dt.getMonth()].y++;
